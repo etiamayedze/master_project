3e84b924d219f91931a5220019a5e024245f72ee
denisjunior <denisdalmeida04@gmail.com>
2022-02-22 14:03:33 +0100

    :white_check_mark: add the registration

---
+++ lib/screens/signup/signup.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+
+  final _formKey =GlobalKey<FormState>();
+  final firstNameEditingController = new TextEditingController();
+  final lastNameEditingController = new TextEditingController();
+  final emailEditingController = new TextEditingController();
+  final passwordEditingController = new TextEditingController();
+  final confirmPasswordEditingController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+
+    final firstNameField =TextFormField(
+      autofocus: false,
+      controller: firstNameEditingController,
+      keyboardType: TextInputType.name,
+      validator: (value){
+        RegExp regex = new RegExp(r'^.{3,}$');
+        if(value!.isEmpty){
+          return("Enterez un prénom! Le champs est obligatoire");
+        }
+        if(!regex.hasMatch(value)){
+          return("Le champs doit être de 5 caractères minimum");
+        }
+        return null;
+      },
+      onSaved: (value){
+        firstNameEditingController.text =value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.account_circle),
+        contentPadding:EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Prénom",
+        border:OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10)
+        )
+      ),
+    );
+
+
+    return Container();
+  }
+}
