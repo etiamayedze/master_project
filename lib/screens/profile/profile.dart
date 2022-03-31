50f422b67529dfdec91456356de78daf23d9c412
denisjunior <denisdalmeida04@gmail.com>
2022-04-01 00:40:41 +0200

    :white_check_mark: add profile ui

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:master_project/screens/profile/profile_screen.dart';
 import 'package:master_project/screens/signup/login.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive/hive.dart';
