67e7e9d8cbcf2da711ec85e3a1baf57d53100825
denisjunior <denisdalmeida04@gmail.com>
2022-04-05 10:45:29 +0200

    :white_check_mark: Start to develop the post and comment functionality

---
+++ lib/screens/post/addPost.dart
@@ -0,0 +1,127 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:master_project/utils/utils.dart';
+class AddPost extends StatefulWidget {
+  const AddPost({Key? key}) : super(key: key);
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+
+  Uint8List? _file;
+  _selectImage(BuildContext context) async {
+    return showDialog(context: context, builder: (context) {
+      return SimpleDialog(
+        title: const Text('Create a post'),
+        children: [
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Take a photo'),
+            onPressed: () async{
+              Navigator.of(context).pop();
+              Uint8List file = await pickImage(
+                ImageSource.camera,
+              );
+              setState(() {
+                _file = file;
+              });
+            },
+          ),
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Choose from gallery '),
+            onPressed: () async{
+              Navigator.of(context).pop();
+              Uint8List file = await pickImage(
+                ImageSource.gallery ,
+              );
+              setState(() {
+                _file = file;
+              });
+            },
+          ),
+        ],
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return _file == null
+        ? Center(
+      child: IconButton(
+        icon: const Icon(Icons.upload),
+        onPressed: () => _selectImage(context),
+      ),
+    )
+        :Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: const Text('Post to'),
+        centerTitle: false,
+        actions: [
+          TextButton(
+              onPressed:() {},
+              child: const Text('post', style: TextStyle(
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),))
+        ],
+
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1649005200470-3ac8cc79a7bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+                ) ,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width*0.4,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Write a caption...',
+                    border: InputBorder.none,
+                  ),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: AspectRatio(
+                  aspectRatio: 487/451,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage('https://images.unsplash.com/photo-1649005200470-3ac8cc79a7bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                        fit: BoxFit.fill,
+                        alignment: FractionalOffset.topCenter,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Divider(),
+            ],
+          ),
+        ],
+      ),
+    );
+
+  }
+}
