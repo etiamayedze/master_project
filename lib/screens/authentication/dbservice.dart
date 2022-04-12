aedcf0f43b2b46f55242f7bb01cfa6e15564b9f5
etiamayedze <etiam.ayedze@gmail.com>
2022-04-12 10:21:06 +0200

    :bug: Found and fix stupid bug

--- lib/screens/authentication/dbservice.dart
+++ lib/screens/authentication/dbservice.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:master_project/data/models/user_model.dart';
 
 class DbServices{
-  var userCollection = FirebaseFirestore.instance.collection('Users');
+  var userCollection = FirebaseFirestore.instance.collection('users');
 
   Stream<List<UserModel>> get getDiscussionUser{
       return userCollection.snapshots().map((event) => event.docs.map((e) => UserModel.fromMap(e.data())).toList());
