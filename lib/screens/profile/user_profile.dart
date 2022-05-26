b153826b2072b22f750a2ae57c6b5fc66c8a5c12
etiamayedze <etiam.ayedze@gmail.com>
2022-05-26 18:35:21 +0200

    :construction: first shot profil page

--- lib/screens/profile/user_profile.dart
+++ lib/screens/profile/user_profile.dart
@@ -26,23 +26,91 @@ class _UserProfileState extends State<UserProfile> {
       body: ListView(
         children: [
           Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-        children: [
-          Row(
-            children: [
-              CircleAvatar(
-                radius: 35,
-                backgroundColor: Colors.black12,
-              ),
-            ],
-          ),
-      ],
-    ),
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 35,
+                      backgroundColor: Colors.black12,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          customedColumn("post", 10),
+                          customedColumn("post", 10),
+                          TextButton.icon(
+                              label: Text(
+                                'Book',
+                                style: GoogleFonts.mochiyPopOne(
+                                  color: CupertinoColors.black,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              icon: Icon(Icons.book,
+                              color: Colors.black,),
+                              onPressed: () {
+                                print('Pressed');
+                              })
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: TextButton(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.blue,
+
+                          ),
+                          alignment: Alignment.center,
+                          child: Text('ok'),
+                          height: 27,
+                          width: 125,
+                        ),
+                        onPressed: (){},
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
+    );
+  }
 
+  Column customedColumn(String label, [int? num]) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          num.toString(),
+          style: GoogleFonts.mochiyPopOne(
+            color: CupertinoColors.black,
+            fontSize: 15,
+          ),
+        ),
+        Text(
+          label,
+          style: GoogleFonts.mochiyPopOne(
+            color: CupertinoColors.black,
+            fontSize: 15,
+          ),
+        ),
+      ],
     );
   }
 }
