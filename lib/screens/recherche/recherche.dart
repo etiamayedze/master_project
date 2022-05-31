7db996ecb6d94c88a54f19c2b7597e30e8b5979e
etiamayedze <etiam.ayedze@gmail.com>
2022-05-31 10:19:56 +0200

    :construction: search in progress

--- lib/screens/recherche/recherche.dart
+++ lib/screens/recherche/recherche.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 class Recherche extends StatefulWidget {
   const Recherche({Key? key}) : super(key: key);
 
@@ -7,8 +10,58 @@ class Recherche extends StatefulWidget {
 }
 
 class _RechercheState extends State<Recherche> {
+  final TextEditingController rechercheController = TextEditingController();
+  bool montreUser = false;
   @override
+  void dispose(){
+    super.dispose();
+    rechercheController.dispose();
+  }
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: TextFormField(
+          controller: rechercheController,
+          decoration: InputDecoration(
+            labelText: 'Recherche',
+
+          ),
+          onFieldSubmitted: (String _){
+            setState(() {
+              montreUser = true;
+            });;
+          },
+        ),
+      ),
+      body: montreUser? StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('users').where('nom', isGreaterThanOrEqualTo: rechercheController.text).snapshots(),
+          builder: (context, snapshot){
+          if (!snapshot.hasData){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+              itemCount: (snapshot.data! as dynamic).docs.length,
+              itemBuilder: (context, index){
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        (snapshot.data! as dynamic).docs[index]['imgUrl'],
+                    ),
+                  ),
+                  title: Text((snapshot.data! as dynamic).docs[index]['nom'],
+                      style: GoogleFonts.mochiyPopOne(
+                        color: CupertinoColors.black,
+                        fontSize: 20,
+                      )
+                  ),
+                );
+              }
+              );
+        },
+      ): Text("No Users"),
+    );
   }
 }
