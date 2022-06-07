6c37427b0ff496f652fd3ba6c20e2b44556bfa69
etiamayedze <etiam.ayedze@gmail.com>
2022-06-07 12:06:57 +0200

    :pencil2::lipstick: fix typo in comment section and add lottie to post page

--- lib/screens/post/addPost.dart
+++ lib/screens/post/addPost.dart
@@ -1,16 +1,17 @@
-
 import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:lottie/lottie.dart';
 import 'package:master_project/providers/firestore_methods.dart';
 import 'package:master_project/utils/utils.dart';
 import 'package:provider/provider.dart';
 
 import '../../data/models/user_model.dart';
 import '../profile/components/profile_menu.dart';
+
 class AddPost extends StatefulWidget {
   const AddPost({Key? key}) : super(key: key);
 
@@ -19,107 +20,103 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
-
   Uint8List? _file;
   bool _isLoading = false;
   final TextEditingController _descriptionController = TextEditingController();
 
   _selectImage(BuildContext context) async {
-    return showDialog(context: context, builder: (context) {
-      return SimpleDialog(
-        title: const Text('Create a post'),
-        children: [
-          SimpleDialogOption(
-            padding: const EdgeInsets.all(20),
-            child: const Text('Take a photo'),
-            onPressed: () async{
-              Navigator.of(context).pop();
-              Uint8List file = await pickImage(
-                ImageSource.camera,
-              );
-              setState(() {
-                _file = file;
-              });
-            },
-          ),
-          SimpleDialogOption(
-            padding: const EdgeInsets.all(20),
-            child: const Text('Choose from gallery '),
-            onPressed: () async{
-              Navigator.of(context).pop();
-              Uint8List file = await pickImage(
-                ImageSource.gallery ,
-              );
-              setState(() {
-                _file = file;
-              });
-            },
-          ),
-          SimpleDialogOption(
-            padding: const EdgeInsets.all(20),
-            child: const Text('Cancel'),
-            onPressed: () async{
-              Navigator.of(context).pop();
-            },
-          ),
-
-        ],
-      );
-    });
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Create a post'),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take a photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(
+                    ImageSource.camera,
+                  );
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Choose from gallery '),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(
+                    ImageSource.gallery,
+                  );
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Cancel'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 
-
-
-  void postImage(String? uid, String? username, String? profImage)async{
-    setState((){
+  void postImage(String? uid, String? username, String? profImage) async {
+    setState(() {
       _isLoading = true;
     });
-    try{
+    try {
       String res = await FirestoreMethods().uploadPost(
-          _descriptionController.text,
-          _file!,
-          uid!,
+        _descriptionController.text,
+        _file!,
+        uid!,
         username!,
         profImage!,
       );
-      if(res == "success"){
-        setState((){
+      if (res == "success") {
+        setState(() {
           _isLoading = false;
         });
-        showSnacBar(context,'Posted!');
+        showSnacBar(context, 'Posted!');
         clearImage();
-      }else{
-        setState((){
+      } else {
+        setState(() {
           _isLoading = false;
         });
         showSnacBar(context, res);
       }
-    }catch(err){
-      showSnacBar(context,err.toString());
+    } catch (err) {
+      showSnacBar(context, err.toString());
     }
-
   }
 
-  void clearImage(){
-    setState((){
+  void clearImage() {
+    setState(() {
       _file = null;
     });
   }
 
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
     _descriptionController.dispose();
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _getActualUser();
   }
 
-
-  User? user = FirebaseAuth.instance.currentUser ;
+  User? user = FirebaseAuth.instance.currentUser;
   FirebaseStorage storage = FirebaseStorage.instance;
   UserModel loginUser = UserModel();
   _getActualUser() async {
@@ -127,96 +124,116 @@ class _AddPostState extends State<AddPost> {
         .collection("users")
         .doc(user!.uid)
         .get()
-        .then((value){
-      this.loginUser =UserModel.fromMap(value.data());
-
+        .then((value) {
+      this.loginUser = UserModel.fromMap(value.data());
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return _file == null
         ? Center(
-      child: IconButton(
-        icon: const Icon(Icons.upload),
-        onPressed: () => _selectImage(context),
-      ),
-    )
-        :Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed:clearImage,
-        ),
-        title: const Text('Post to'),
-        centerTitle: false,
-        actions: [
-          TextButton(
-              onPressed: () => postImage(
-                  loginUser.uid,
-                  loginUser.nom,
-                  loginUser.imgUrl
-              ),
-              child: const Text('post', style: TextStyle(
-                color: Colors.blueAccent,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),))
-        ],
-
-      ),
-      body: Column(
-        children: [
-          _isLoading
-              ? const LinearProgressIndicator()
-              : const Padding(
-            padding: EdgeInsets.only(top: 0),
-          ),
-          const Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "${loginUser.imgUrl}"
-                ) ,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width*0.4,
-                child: TextField(
-                  controller: _descriptionController,
-                  decoration: const InputDecoration(
-                    hintText: 'Ajouter une légende...',
-                    border: InputBorder.none,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 200,
+                          child: Lottie.asset(
+                            "assets/upload.json",
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.upload),
+                        onPressed: () => _selectImage(context),
+                      ),
+                    ],
                   ),
-                  maxLines: 8,
-                ),
+                ],
               ),
-              SizedBox(
-                height: 45,
-                width: 45,
-                child: AspectRatio(
-                  aspectRatio: 487/451,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: MemoryImage(_file!),
-                        fit: BoxFit.fill,
-                        alignment: FractionalOffset.topCenter,
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: clearImage,
+              ),
+              title: const Text('Post to'),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                    onPressed: () => postImage(
+                        loginUser.uid, loginUser.nom, loginUser.imgUrl),
+                    child: const Text(
+                      'post',
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ))
+              ],
+            ),
+            body: Column(
+              children: [
+                _isLoading
+                    ? const LinearProgressIndicator()
+                    : const Padding(
+                        padding: EdgeInsets.only(top: 0),
+                      ),
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage("${loginUser.imgUrl}"),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: TextField(
+                        controller: _descriptionController,
+                        decoration: const InputDecoration(
+                          hintText: 'Ajouter une légende...',
+                          border: InputBorder.none,
+                        ),
+                        maxLines: 8,
                       ),
                     ),
-                  ),
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: MemoryImage(_file!),
+                              fit: BoxFit.fill,
+                              alignment: FractionalOffset.topCenter,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Divider(),
+                  ],
                 ),
-              ),
-              const Divider(),
-            ],
-          ),
-        ],
-      ),
-    );
-
+              ],
+            ),
+          );
   }
 }
